b2ec672329f631f760b599c7d7dfcccb2833cbbc
emreduman3000 <emryduman@gmail.com>
2023-01-07 11:52:01 -0600

    7 January 2023 propertiesFile, PDFFile, takingScreenShot, videoRecorder

---
+++ src/test/java/com/testng/testbase_loglama_propertiesReadWrite_takeSS/d_testbase_loglama_properties_takeSSandVideo/utilities/FileUtil.java
@@ -0,0 +1,41 @@
+package com.testng.testbase_loglama_propertiesReadWrite_takeSS.d_testbase_loglama_properties_takeSSandVideo.utilities;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.testng.testbase_loglama_propertiesReadWrite_takeSS.d_testbase_loglama_properties_takeSSandVideo.utilities.Constants.*;
+
+public class FileUtil
+{
+
+    public static void refreshTestOutput()
+    {
+        try {
+            File f = new File(TEST_OUTPUT_PATH);
+
+            if(f.exists())//boyle bir path'te file var ise true return eder
+            {
+                FileUtils.cleanDirectory(f); //Optional-TEST-OUTPUT DIRECTORY'SININ ICINI SILER
+
+                //TEST-OUTPUT DIRECTORY'SINI SILER
+                FileUtils.forceDelete(f);
+            }
+
+            if(!f.exists()){
+                //SCREENSHOTS_PATH VE SCREENRECORDING_PATH DIRECTORY'LERINI OLUSTURUR
+                FileUtils.forceMkdir(new File(SCREENSHOTS_PATH));
+                FileUtils.forceMkdir(new File(SCREENRECORDING_PATH));
+                FileUtils.forceMkdir(new File(PDFPATH_SCREENSHOTS));
+            }
+                    }
+        catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
